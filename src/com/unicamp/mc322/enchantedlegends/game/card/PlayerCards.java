6efab3fac85a9cc851d2f6962dbeb68279cb4daa
aasatorres <aa.satorres@gmail.com>
2021-06-18 21:46:42 -0300

    [EL-8] - <<<<

--- src/com/unicamp/mc322/enchantedlegends/game/card/PlayerCards.java
+++ src/com/unicamp/mc322/enchantedlegends/game/card/PlayerCards.java
@@ -3,10 +3,6 @@ package com.unicamp.mc322.enchantedlegends.game.card;
 import java.util.List;
 
 public class PlayerCards {
-<<<<<<< HEAD
-
-=======
->>>>>>> main
     private List<Card> onDeck;
     private List<Card> onHand;
     private List<Card> units;
