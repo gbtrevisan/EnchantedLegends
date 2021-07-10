b1a70851bd40e92d77e5a62dba1329403e14f8b3
Gabriel Luiz Trevisan <g.luiztrevisan@gmail.com>
2021-07-10 14:34:11 -0300

    remove merge messages in code

--- src/com/unicamp/mc322/enchantedlegends/game/card/unit/champion/types/ReciveDamageToLevelUpChampion.java
+++ src/com/unicamp/mc322/enchantedlegends/game/card/unit/champion/types/ReciveDamageToLevelUpChampion.java
@@ -1,13 +1,8 @@
 package com.unicamp.mc322.enchantedlegends.game.card.unit.champion.types;
 
-<<<<<<< HEAD
-import com.unicamp.mc322.enchantedlegends.game.card.trait.Trait;
-import com.unicamp.mc322.enchantedlegends.game.card.trait.TraitException;
-=======
 import com.unicamp.mc322.enchantedlegends.game.card.effect.Effect;
 import com.unicamp.mc322.enchantedlegends.game.card.unit.trait.Trait;
 import com.unicamp.mc322.enchantedlegends.game.card.unit.trait.exception.TraitException;
->>>>>>> main
 import com.unicamp.mc322.enchantedlegends.game.card.unit.Follower;
 import com.unicamp.mc322.enchantedlegends.game.card.unit.champion.Champion;
 import com.unicamp.mc322.enchantedlegends.game.card.unit.champion.upgrades.ChampionUpgrade;
